6b28b3f24c98ca165b6f60ed9183dd29a719b60d
theqihao <theqihao@hust.edu.cn>
2022-05-06 22:34:31 +0800

    add mem

--- examples/qihao.cpp
+++ examples/qihao.cpp
@@ -6,17 +6,20 @@
  ************************************************************************/
 
 #include "qihao.h"
+#include "mem.h"
 
+#include<unistd.h>
 #include<iostream>
 using namespace std;
 
-const int N = 10000;
+const int N = 10000000;
 
 int main(int argc, const char *argv[])
 {
-	int i,a[N],b[N],c[N];
+	int a[N],b[N],c[N];
 	uint32_t total = 0;
 	int lcore_id;
+    uint64_t i;
 
 	srand(0);
 	for (i=0; i < N; ++i) {
@@ -24,18 +27,30 @@ int main(int argc, const char *argv[])
 		b[i] = rand();
 		c[i] = rand();
 	}
+    bool encpu = true;
+    bool enmem = true;
+    QIHAO* qihao = NULL;
+    MEM* mem = NULL;
 
-    QIHAO* qihao = new QIHAO();
-    qihao->init();
-    qihao->start();
-	for(i=0;i<100000;i++)
+    if (encpu) qihao = new QIHAO();
+    if (enmem) mem = new MEM();
+    if (encpu) qihao->init();
+    if (enmem) mem->init();
+    if (encpu) qihao->start();
+    if (enmem) mem->start();
+    sleep(2);
+	for(i=0;i<10000000000;i++)
 		c[i%N] = 4 * a[i%N] + b[i%N];
 	for(i=0;i<N;i++)
 		total += c[i];
-    qihao->stop();
+    if (encpu) qihao->stop();
+    if (enmem) mem->stop();
 
-    qihao->print();
+    if (encpu) qihao->print();
+    if (enmem) mem->print();
 
+    if (encpu) delete qihao;
+    if (enmem) delete mem;
     return 0;
 
 }
